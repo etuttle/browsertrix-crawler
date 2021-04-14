4e5c5bc1d1d7613da4aefbb0d63972dda8bf90a1
Emma Dickson <emmadickson@Emmas-MacBook-Pro.local>
2021-04-14 16:08:43 -0400

    update tests

--- tests/url_file_list.test.js
+++ tests/url_file_list.test.js
@@ -1,21 +1,21 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
+const fs = require("fs");
 
 test("check that all urls in a file list are crawled when the filelisturl param is passed", async () => {
   jest.setTimeout(30000);
-  let passed = "";
 
   try{
-    const data = await exec("docker-compose run crawler crawl --url http://www.example.com/ --collection filelisttest --urlFileList fixtures/urlSeedFile.txt");
+    await exec("docker-compose run crawler crawl --url http://www.example.com/ --collection filelisttest --urlFileList fixtures/urlSeedFile.txt");
   }
   catch (error) {
-    passed = false;
+    console.log(error);
   }
-  datapackage = JSON.parse(fs.readFileSync("crawls/collections/filelisttest/datapackage.json", "utf8")
-  seed_file = fs.readFileSync("tests/fixtures/urlSeedFile", "utf8").split("\n").sort()
-  crawled_pages = []
-  for (var i = 0; i < datapackage['resources'].length; i++) {
-    crawled_pages.push(datapackage['resources'][i]['url'])
+  let datapackage = JSON.parse(fs.readFileSync("crawls/collections/filelisttest/datapackage.json", "utf8"));
+  let seed_file = fs.readFileSync("tests/fixtures/urlSeedFile", "utf8").split("\n").sort();
+  let crawled_pages = [];
+  for (var i = 0; i < datapackage["resources"].length; i++) {
+    crawled_pages.push(datapackage["resources"][i]["url"]);
   }
   expect(crawled_pages.sort()).toBe(seed_file);
 });
